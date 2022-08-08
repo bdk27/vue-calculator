f450538b5b396699876f2a132eb58fd9477b86cb
BDK27 <briansam195@gmail.com>
2022-08-08 18:31:11 +0800

    finish

--- vue.js
+++ vue.js
@@ -1,9 +1,46 @@
 const app = {
     data() {
-
+        return {
+            num1: '',
+            num2: '',
+            op: '',
+            ans: '',
+            result: '',
+            history: [],
+        }
     },
     methods: {
-
+        plus() {
+            this.ans = this.num1 + this.num2;
+            this.op = '+';
+        },
+        minus() {
+            this.ans = this.num1 - this.num2;
+            this.op = '-';
+        },
+        times() {
+            this.ans = this.num1 * this.num2;
+            this.op = '*';
+        },
+        divided() {
+            this.ans = this.num1 / this.num2;
+            this.op = '/';
+        },
+        output() {
+            this.result = this.ans;
+            if (this.history.length < 3){
+                this.history.push( this.num1 + ' ' + this.op + ' ' + this.num2 + ' = ' + this.result);
+            }else {
+                this.history.shift();
+                this.history.push( this.num1 + ' ' + this.op + ' ' + this.num2 + ' = ' + this.result);
+            }
+            
+        },
+        clear() {
+            this.num1 = '';
+            this.num2 = '';
+            this.result = '';
+        }
     },
     mounted() {
         
